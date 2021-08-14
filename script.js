426b953e862dee870a02f208a715d11d2fcdd41d
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-08-14 22:29:42 +0800

    3m 12k

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 if (location.hash == "#custom") {
     document.getElementsByClassName("main")[0].style.display = "none";
     document.getElementsByClassName("cust")[0].style.display = "block";
+    document.getElementsByClassName("game")[0].style.display = "none";
+}else if(location.hash!="#"&&location.hash!=""){
+    document.getElementsByClassName("main")[0].style.display = "none";
+    document.getElementsByClassName("cust")[0].style.display = "none";
+    document.getElementsByClassName("game")[0].style.display = "block";
 }
 function chas(a) {
     location.hash = "#" + a;
     history.go(0);
+}
+function goto(a, b, c) {
+    chas(a + "," + b + "," + c);
 }

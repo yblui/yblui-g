b996663f2a471c17d62a4da6bee6974b5bab22b2
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-08-16 21:43:06 +0800

    3m 18k

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var wdj = []
+var wdj = [], tim = 0, jshi;
+if(!document.cookie) document.cookie="theme=false;expires=Thu, 18 Dec 2043 12:00:00 GMT";
 if (location.hash == "#custom") {
     document.getElementsByClassName("main")[0].style.display = "none";
     document.getElementsByClassName("cust")[0].style.display = "flex";
+} else if (location.hash == "#theme") {
+    document.getElementsByClassName("main")[0].style.display = "none";
+    document.getElementsByClassName("them")[0].style.display = "flex";
     document.getElementsByClassName("game")[0].style.display = "none";
 } else if (location.hash != "#" && location.hash != "") {
     document.getElementsByClassName("main")[0].style.display = "none";
     document.getElementsByClassName("cust")[0].style.display = "none";
     document.getElementsByClassName("game")[0].style.display = "block";
-    var list = location.hash.replace("#", "").split(",");
+    var list = location.hash.replace("#", "").split(","), lei = list[2];
     for (var a = 0; a < Number(list[0]); a++) {
         for (var b = 0; b < Number(list[1]); b++) {
-            document.getElementsByClassName("game")[0].innerHTML += "<div class='mine grid a" + a + " b" + b + "' onclick='cheq(this)' data-a='" + a + "' data-b='" + b + "'></div>";
+            document.getElementsByClassName("game")[0].innerHTML += "<div class='mine grid a" + a + " b" + b + "' onclick='cheq(this)' data-a='" + a +
+                "' data-b='" + b + "' onmousedown='biao(this,event)'></div>";
         }
         document.getElementsByClassName("game")[0].innerHTML += "<br />";
     }
@@ -18,7 +23,12 @@ if (location.hash == "#custom") {
         var tem = Math.floor(Math.random() * document.getElementsByClassName("mine").length);
         document.getElementsByClassName("mine")[tem].className = document.getElementsByClassName("mine")[tem].className.replace("mine", "");
     }
+    document.getElementById("syls").innerText = lei;
 }
+setInterval(() => {
+    cheq(wdj[0]);
+    wdj.shift();
+}, 10);
 function chas(a) {
     location.hash = "#" + a;
     history.go(0);
@@ -38,20 +48,22 @@ function cheq(a) {
             a.style.backgroundColor = "white";
             for (var lsc = -1; lsc <= 1; lsc++) {
                 for (var lsd = -1; lsd <= 1; lsd++) {
-                    var pd=true;
-                    for(var lse=0;lse<wdj.length;lse++){
-                        if(wdj[lse]==document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0])pd=false
+                    var pd = true;
+                    for (var lse = 0; lse < wdj.length; lse++) {
+                        if (wdj[lse] == document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0]) pd = false
                     }
-                    if (document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0] && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].style.backgroundColor != "white" && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].style.backgroundColor != "#E6C460" && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].innerText != "X"&&pd) wdj[wdj.length] = document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0];
+                    if (document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0] && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].style.backgroundColor != "white" && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].style.backgroundColor != "#E6C460" && document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0].innerText != "X" && pd) wdj[wdj.length] = document.getElementsByClassName("a" + (Number(a.dataset.a) + lsc) + " b" + (Number(a.dataset.b) + lsd))[0];
                 }
             }
         }
+        if (!jshi) {
+            jshi = setInterval(function () {
+                tim++;
+                document.getElementById("time").innerText = tim + "s";
+            }, 1000)
+        }
     }
 }
-setInterval(() => {
-    cheq(wdj[0]);
-    wdj.shift();
-}, 10);
 function arou(a, b) {
     var coun = 0;
     for (var lsc = -1; lsc <= 1; lsc++) {
@@ -63,4 +75,23 @@ function arou(a, b) {
 }
 function qdmi() {
     document.getElementById("mine").max = Number(document.getElementById("len").value) * Number(document.getElementById("wdt").value);
+}
+function biao(a, event) {
+    if (event.which == 3 && !a.innerText) {
+        a.innerText = "?";
+        lei--;
+    } else if (a.innerText == "?") {
+        a.innerText = "";
+        lei++;
+    }
+    document.getElementById("syls").innerText = lei;
+}
+function cthe(a){
+    if(a){
+        document.getElementsByTagName("html")[0].classList.add("green");
+        document.getElementsByTagName("html")[0].classList.remove("blue");
+    } else {
+        document.getElementsByTagName("html")[0].classList.remove("green");
+        document.getElementsByTagName("html")[0].classList.add("blue");
+    }
 }
